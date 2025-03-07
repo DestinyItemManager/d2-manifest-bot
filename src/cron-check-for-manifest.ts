69bc6af007a47ca44fed83270729e79b3e7286ca
Ben Hollis <ben@benhollis.net>
2025-03-06 18:31:36 -0800

    Publish to dim-custom-symbols again

--- src/cron-check-for-manifest.ts
+++ src/cron-check-for-manifest.ts
@@ -34,70 +34,46 @@ const skipCheck = process.env.SKIP_CHECK === 'true' ? true : false;
 
   // if (!/^[.\w-]+$/.test(versionNumber)) { I AM NOT REALLY SURE THIS NEEDS DOING. }
 
-  const buildOptions = {
-    url: 'https://api.github.com/repos/DestinyItemManager/d2-additional-info/dispatches',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: 'Basic ' + Buffer.from(process.env.PAT || '').toString('base64'),
-      'User-Agent': 'd2-additional-info',
-    },
-    body: JSON.stringify({
-      event_type: 'new-manifest-detected',
-      client_payload: {
-        message: buildMessage,
-        branch: 'master',
-        config: {
-          env: {
-            MANIFEST_VERSION: current,
+  const promises: Promise<void>[] = [];
+
+  for (const repo of ['d2-additional-info', 'dim-custom-symbols']) {
+    promises.push(
+      (async () => {
+        const buildOptions = {
+          url: `https://api.github.com/repos/DestinyItemManager/${repo}/dispatches`,
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: 'Basic ' + Buffer.from(process.env.PAT || '').toString('base64'),
+            'User-Agent': 'd2-additional-info',
           },
-        },
-      },
-    }),
-    json: true,
-    method: 'POST',
-  };
+          body: JSON.stringify({
+            event_type: 'new-manifest-detected',
+            client_payload: {
+              message: buildMessage,
+              branch: 'master',
+              config: {
+                env: {
+                  MANIFEST_VERSION: current,
+                },
+              },
+            },
+          }),
+          json: true,
+          method: 'POST',
+        };
 
-  /* const buildOptions2 = {
-    url: 'https://api.github.com/repos/DestinyItemManager/dim-custom-symbols/dispatches',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: 'Basic ' + btoa(process.env.PAT || ''),
-      'User-Agent': 'd2-additional-info',
-    },
-    body: JSON.stringify({
-      event_type: 'new-manifest-detected',
-      client_payload: {
-        message: buildMessage,
-        branch: 'main',
-        config: {
-          env: {
-            MANIFEST_VERSION: current,
-          },
-        },
-      },
-    }),
-    json: true,
-    method: 'POST',
-  };
-  */
-
-  const githubFetch = await fetch(buildOptions.url, buildOptions);
-  //const githubFetch2 = await fetch(buildOptions2.url, buildOptions2);
-
-  if (!githubFetch.ok) {
-    console.log('Github returned an error');
-    console.log(githubFetch);
-    process.exit(1);
+        const githubFetch = await fetch(buildOptions.url, buildOptions);
+        if (!githubFetch.ok) {
+          console.log('Github returned an error pinging', repo);
+          console.log(githubFetch);
+          process.exit(1);
+        }
+      })(),
+    );
   }
-  /*
-  if (!githubFetch2.ok) {
-    console.log('Github returned an error');
-    console.log(githubFetch2);
-    process.exit(1);
-  }
-  */
+
+  await Promise.allSettled(promises);
 })().catch((e) => {
   console.log(e);
   process.exit(1);
